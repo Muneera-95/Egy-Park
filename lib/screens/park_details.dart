56fe00e36d2a509e93588cbef35206dbd7c5d2ad
Muneera-95 <87190640+Muneera-95@users.noreply.github.com>
2025-02-16 02:00:38 +0300

    Update park_details.dart

--- lib/screens/park_details.dart
+++ lib/screens/park_details.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:egy_park/widgets/bottom_sheet.dart';
-import 'package:egy_park/widgets/floating_appbar.dart';
-import 'package:egy_park/widgets/park_slot.dart';
+import 'package:Q_park/widgets/bottom_sheet.dart';
+import 'package:Q_park/widgets/floating_appbar.dart';
+import 'package:Q_park/widgets/park_slot.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
